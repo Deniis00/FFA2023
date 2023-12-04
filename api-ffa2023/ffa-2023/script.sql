4052ed3f91f15c09493cf1b91c5c86d7ce577d1e
denis.ayala <denis.ayala@PC-DENIS>
2023-12-04 15:08:10 -0300

    app

--- api-ffa2023/ffa-2023/script.sql
+++ api-ffa2023/ffa-2023/script.sql
@@ -2,6 +2,8 @@
 
 -- ffa2023.funcionarios definition
 
+-- ffa2023.funcionarios definition
+
 CREATE TABLE `funcionarios` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `nombre_funcionario` varchar(255) NOT NULL,
@@ -14,8 +16,12 @@ CREATE TABLE `funcionarios` (
   `hora_llegada` time DEFAULT NULL,
   `sorteado` bigint DEFAULT '0',
   `fecha_hora_sorteado` datetime DEFAULT NULL,
+  `participa_sorteo2` tinyint DEFAULT '1',
+  `registro_entrada2` bigint DEFAULT '0',
+  `sorteado2` bigint DEFAULT '0',
+  `fecha_hora_sorteado2` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=202 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=700 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
 CREATE DEFINER=`root`@`localhost` TRIGGER `funcionarios_upd_before` BEFORE UPDATE ON `funcionarios` FOR EACH ROW begin
@@ -27,4 +33,9 @@ CREATE DEFINER=`root`@`localhost` TRIGGER `funcionarios_upd_before` BEFORE UPDAT
      IF OLD.sorteado = 0 AND NEW.sorteado = 1 THEN
           set new.fecha_hora_sorteado = current_time();
      END IF;
+
+     IF OLD.sorteado2 = 0 AND NEW.sorteado2 = 1 THEN
+               set new.fecha_hora_sorteado2 = current_time();
+     END IF;
 end;
+
